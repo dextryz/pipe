9bce8aeae5270dbe78dce6dfe06db7ae59d2224b
dextryz <rvs.hello@pm.me>
2024-02-04 09:24:19 -0500

    add publish to pipeline

--- main.go
+++ main.go
@@ -15,15 +15,6 @@ import (
 	"github.com/nbd-wtf/go-nostr/nip19"
 )
 
-type kv struct {
-	Key   string
-	Value int
-}
-
-type Events struct {
-	EventList []*nostr.Event `json:"events"`
-}
-
 // Only connect to a single relay for the time being.
 // We have to look at the Pool impl in go-nostr.
 // The problem is pulling the same event from multiple relays.
@@ -39,6 +30,34 @@ func StringEnv(key string) string {
 var (
 	PRIVATE_KEY = StringEnv("PRIVATE_KEY")
 )
+
+type kv struct {
+	Key   string
+	Value int
+}
+
+type Events struct {
+	EventList []*nostr.Event `json:"events"`
+}
+
+func (s *Events) Serialize() []byte {
+	b := []byte("{\"events\":[")
+	for i, evt := range s.EventList {
+		if i > 0 {
+			b = append(b, ',')
+		}
+
+		be, err := json.Marshal(evt)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		b = append(b, be...)
+	}
+	b = append(b, ']')
+	b = append(b, '}')
+    return b
+}
 
 type Pipeline struct {
 	Relay  *nostr.Relay
@@ -62,26 +81,59 @@ func New(relay string) *Pipeline {
 	}
 }
 
-func (s *Pipeline) Author(npub string) *Pipeline {
-	_, pk, err := nip19.Decode(npub)
-	if err != nil {
-		panic(err)
+func (s *Pipeline) Kinds(kinds []int) *Pipeline {
+
+	var filter nostr.Filter
+	input := bufio.NewScanner(s.Reader)
+	for input.Scan() {
+		err := json.Unmarshal(input.Bytes(), &filter)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+		}
 	}
 
-	f := nostr.Filter{
-		Kinds:   []int{nostr.KindArticle},
-		Authors: []string{pk.(string)},
-		Limit:   10,
-	}
+	filter.Kinds = kinds
 
-	bb, err := json.Marshal(f)
+	data, err := json.Marshal(filter)
 	if err != nil {
-		fmt.Println("Error serializing filter:", err)
+		fmt.Println("error serializing filter:", err)
 		return nil
 	}
+	var b bytes.Buffer
+	b.Write(data)
 
-	var b bytes.Buffer // A Buffer needs no initialization.
-	b.Write(bb)
+	return &Pipeline{
+		Relay:  s.Relay,
+		Reader: &b,
+		Output: s.Output,
+	}
+}
+
+func (s *Pipeline) Authors(npubs []string) *Pipeline {
+
+    var authors []string
+
+    for _, npub := range npubs {
+        _, pk, err := nip19.Decode(npub)
+        if err != nil {
+            panic(err)
+        }
+        authors = append(authors, pk.(string))
+    }
+
+	f := nostr.Filter{
+		Kinds:   []int{nostr.KindTextNote},
+		Authors: authors,
+		Limit:   100,
+	}
+
+	data, err := json.Marshal(f)
+	if err != nil {
+		fmt.Println("error serializing filter:", err)
+		return nil
+	}
+	var b bytes.Buffer
+	b.Write(data)
 
 	return &Pipeline{
 		Relay:  s.Relay,
@@ -92,12 +144,9 @@ func (s *Pipeline) Author(npub string) *Pipeline {
 
 func (s *Pipeline) Query() *Pipeline {
 
-	//result := &bytes.Buffer{}
-	input := bufio.NewScanner(s.Reader)
-
 	var filter nostr.Filter
+	input := bufio.NewScanner(s.Reader)
 	for input.Scan() {
-
 		err := json.Unmarshal(input.Bytes(), &filter)
 		if err != nil {
 			fmt.Println("Error decoding JSON:", err)
@@ -110,24 +159,11 @@ func (s *Pipeline) Query() *Pipeline {
 		log.Fatalln(err)
 	}
 
-	serialized := []byte("{\"events\":[")
-	for i, evt := range events {
-		if i > 0 {
-			serialized = append(serialized, ',')
-		}
-
-		be, err := json.Marshal(evt)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		serialized = append(serialized, be...)
-	}
-	serialized = append(serialized, ']')
-	serialized = append(serialized, '}')
-
+    el := Events{
+        EventList: events,
+    }
 	var b bytes.Buffer
-	b.Write(serialized)
+	b.Write(el.Serialize())
 
 	return &Pipeline{
 		Relay:  s.Relay,
@@ -138,11 +174,9 @@ func (s *Pipeline) Query() *Pipeline {
 
 func (s *Pipeline) Tags() *Pipeline {
 
-	input := bufio.NewScanner(s.Reader)
-
 	var events Events
+	input := bufio.NewScanner(s.Reader)
 	for input.Scan() {
-
 		err := json.Unmarshal(input.Bytes(), &events)
 		if err != nil {
 			fmt.Println("Error decoding JSON:", err)
@@ -151,7 +185,6 @@ func (s *Pipeline) Tags() *Pipeline {
 	}
 
 	tags := make(map[string]int)
-
 	for _, e := range events.EventList {
 		for _, t := range e.Tags {
 			if t.Key() == "t" {
@@ -169,7 +202,6 @@ func (s *Pipeline) Tags() *Pipeline {
 	if err != nil {
 		log.Fatalf("Error marshaling map to JSON: %v", err)
 	}
-
 	var b bytes.Buffer
 	b.Write(data)
 
@@ -183,9 +215,7 @@ func (s *Pipeline) Tags() *Pipeline {
 func (s *Pipeline) SortByCount() *Pipeline {
 
 	tags := make(map[string]int)
-
 	input := bufio.NewScanner(s.Reader)
-
 	for input.Scan() {
 		err := json.Unmarshal(input.Bytes(), &tags)
 		if err != nil {
@@ -209,7 +239,6 @@ func (s *Pipeline) SortByCount() *Pipeline {
 	if err != nil {
 		log.Fatalf("Error marshaling sorted map to JSON: %v", err)
 	}
-
 	var b bytes.Buffer
 	b.Write(data)
 
@@ -223,9 +252,7 @@ func (s *Pipeline) SortByCount() *Pipeline {
 func (s *Pipeline) SortByName() *Pipeline {
 
 	tags := make(map[string]int)
-
 	input := bufio.NewScanner(s.Reader)
-
 	for input.Scan() {
 		err := json.Unmarshal(input.Bytes(), &tags)
 		if err != nil {
@@ -254,7 +281,6 @@ func (s *Pipeline) SortByName() *Pipeline {
 	if err != nil {
 		log.Fatalf("Error marshaling sorted map to JSON: %v", err)
 	}
-
 	var b bytes.Buffer
 	b.Write(data)
 
@@ -262,6 +288,51 @@ func (s *Pipeline) SortByName() *Pipeline {
 		Relay:  s.Relay,
 		Reader: &b,
 		Output: s.Output,
+	}
+}
+
+func (s *Pipeline) Publish(relay string) {
+
+	ctx := context.Background()
+
+	r, err := nostr.RelayConnect(ctx, relay)
+	if err != nil {
+		panic(err)
+	}
+
+	events := Events{}
+	input := bufio.NewScanner(s.Reader)
+	for input.Scan() {
+		err := json.Unmarshal(input.Bytes(), &events)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+		}
+	}
+
+	// There has to be events cached in the buffer.
+	// It makes no sense to publish an empty buffer no then does it?
+	// Impl proper cheks and balances
+	for _, e := range events.EventList {
+
+		event := &nostr.Event{
+			Kind:      e.Kind,
+			Content:   e.Content + "AFTER PIPELINE",
+			CreatedAt: nostr.Now(),
+		}
+
+		_, sk, err := nip19.Decode(PRIVATE_KEY)
+		if err != nil {
+			panic(err)
+		}
+
+		event.Sign(sk.(string))
+
+		err = r.Publish(ctx, *event)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Println("Published")
 	}
 }
 
@@ -288,7 +359,8 @@ func main() {
 	npub := "npub14ge829c4pvgx24c35qts3sv82wc2xwcmgng93tzp6d52k9de2xgqq0y4jk"
 
 	pipeline := New("wss://relay.damus.io/")
-	pipeline.Author(npub).Query().Tags().SortByCount().Stdout()
+	pipeline.Authors([]string{npub}).Kinds([]int{nostr.KindArticle}).Query().Tags().SortByCount().Stdout()
+	//pipeline.Authors([]string{npub}).Query().Publish("wss://relay.damus.io/")
 
 	//p.Author(npub).Kind(nostr.KindTextNote).Query().Stdout()
 	//p.Author(npub).Kind(nostr.KindTextNote).Query().Publish("wss://relay.damus.io/")
